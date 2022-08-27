e3449adc3216df8096514de687970f4d84ea5aab
Ansh Shah <ansh.rs@somaiya.edu>
2022-08-27 12:18:30 +0530

    added new code

--- Python Classes/lec1_typecasting_.py
+++ Python Classes/lec1_typecasting_.py
@@ -1,5 +1,72 @@
-a="Ansh"
-b="Shah"
-c=9812345670
-print(a+" "+b+" "+str(c))
-print(f'Hi, I am {a} {b} and my number is {c}')
+"""
+
+a=5.8
+print(a)
+print(type(a))
+b=int(a)
+print(b)
+print(type(b))
+a=5.8765
+b=float(a)
+print (b)
+z=56
+b="st"
+c=str(z)
+print(b+c)
+"""
+"""F_name= "try"
+L_name= "catch"
+Ph_no= 1234567890
+print (F_name+" "+L_name+" "+str(Ph_no))"""
+
+'''a="789"
+b= 456
+print(f"hi I am {a} and my g is {b}")
+#this is another option.
+c = (f"hi I am {a} and my g is {b}")
+print (c)'''
+
+"""
+#int to float
+g=9
+i=float(g)
+print (type(i))
+    
+#int to strg
+h=56
+k=str(h)
+print (type(k))
+
+#str to int
+r="56"
+l=int(r)
+print(type(l))
+
+#same goes for float
+t="sd"
+j=int(t)
+print(type(j))
+
+d="sd78"
+n= int(d)
+print(type(n))
+
+#float to str
+e=6.778
+s=str(e)
+print(type(s))
+
+#str to float
+v="2.523645"
+x=float(v)
+print(type(x))
+
+#f"" called as f-string is an alternative to type casting where the variable is considered in its original data type and can be used without getting converted.
+#and directly we can print the statement.
+z="sid"
+m=20
+print(f"Hi, I m {z} and my age is {m}")
+#or
+q=f"Hi, I m {z} and my age is {m}"
+print (q)
+"""
